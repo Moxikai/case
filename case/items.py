972e402bba549769b301d99a4d5b5b4df48746ad
Moxikai <zhu-hero@qq.com>
2016-10-12 10:05:32 +0800

    add extract data rules

--- case/items.py
+++ case/items.py
@@ -6,9 +6,20 @@
 # http://doc.scrapy.org/en/latest/topics/items.html
 
 import scrapy
-
+from scrapy import Field,Item
 
 class CaseItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     pass
+    title = Field() # 标题
+    types = Field() # 案件类型，从主要到细分，多个值
+    court = Field() # 审理法院
+    document_code = Field() # 文书字号
+    document_type = Field() # 文书类型
+    conclusion_date = Field() # 审结日期
+    proceeding = Field() # 审理程序，一审、二审等等
+    trial_persons = Field() # 审理人员
+    judgment = Field() # 判决书
+    crawl_date = Field() # 爬虫收录日期
+
--- case/spiders/lawxp.py
+++ case/spiders/lawxp.py
@@ -7,7 +7,10 @@ sys.setdefaultencoding('utf-8')
 import scrapy
 
 from scrapy import Request
+from scrapy.loader import ItemLoader
+from scrapy.loader.processors import TakeFirst,MapCompose,Join
 
+from case.items import CaseItem
 class LawxpSpider(scrapy.Spider):
     name = "lawxp"
     allowed_domains = ["lawxp.com"]
@@ -37,12 +40,42 @@ class LawxpSpider(scrapy.Spider):
 
     def parseDetail(self,response):
         """解析详细信息"""
+        """
 
-
+        l = ItemLoader(item=CaseItem(),response=response)
+        l.add_xpath('title','/html/head/title/text()',TakeFirst,unicode.title) # 标题
+        l.add_xpath('types','//li[@class="mylnr-jj1"][2]/span/a/text()',unicode.title,Join) # 类型，还需转字符串
+        l.add_xpath('court','//li[@class="mylnr-jj1"][3]/span/text()',TakeFirst) # 审理法院
+        l.add_xpath('document_code', '//li[@class="mylnr-jj1"][4]/span[1]/text()', TakeFirst) # 文书字号
+        l.add_xpath('document_type', '//li[@class="mylnr-jj1"][4]/span[2]/text()', TakeFirst) # 文书类型
+        l.add_xpath('conclusion_date','//li[@class="mylnr-jj1"][5]/span[1]/text()',TakeFirst) # 审结日期
+        l.add_xpath('proceeding','//li[@class="mylnr-jj1"][5]/span[2]/text()',TakeFirst) # 审理程序
+        l.add_xpath('judgment','//div[@id="rong_ziId"]',TakeFirst,unicode.title) # 判决书
+        l.load_item()
+        """
         title = response.xpath('/html/head/title/text()').extract_first()
-        print '案例：---------------------%s-----------------------页面下载成功'%(title),'\n'
-        #print response.body
+        type_list = response.xpath('//li[@class="mylnr-jj1"][2]/span/a/text()').extract()
+        types = ' '.join([str(i) for i in type_list])
+        court = response.xpath('//li[@class="mylnr-jj1"][3]/span/text()').extract_first()
+        document_code = response.xpath('//li[@class="mylnr-jj1"][4]/span[1]/text()').extract_first()
+        document_type = response.xpath('//li[@class="mylnr-jj1"][4]/span[2]/text()').extract_first()
+        conclusion_date = response.xpath('//li[@class="mylnr-jj1"][5]/span[1]/text()').extract_first()
+        proceeding = response.xpath('//li[@class="mylnr-jj1"][5]/span[2]/text()').extract_first()
+        judgment = response.xpath('//div[@id="rong_ziId"]').extract_first()
         yield {'title':title,
-               'url':response.url}
+               'types':types,
+               'court':court,
+               'document_code':document_code,
+               'document_type':document_type,
+               'conclusion_date':conclusion_date,
+               'proceeding':proceeding,
+               'judgment':judgment,
+               }
+
+
+
+
+
+
 
 
