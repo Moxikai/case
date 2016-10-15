e8e7c9acbb7975226385fe571b55d632a04673c9
Moxikai <zhu-hero@qq.com>
2016-10-15 08:49:37 +0800

    just commit before start

--- case/spiders/uniform.py
+++ case/spiders/uniform.py
@@ -7,10 +7,18 @@ from scrapy.selector import Selector
 from scrapy import Request
 
 
-
-class UniformSpider(Spider):
+class UniformSpider(CrawlSpider):
     name = "uniform"
-
+    rules = (Rule(LinkExtractor(restrict_xpaths=('//ul[@id="xfgdq"]/li/a/@href')),
+                  follow=True,
+                  process_links='join_url'),
+             Rule(LinkExtractor(restrict_xpaths=('//div[@id="Group_RegionInfo1__RegionLevel2"]/ul/li/a/@href')),
+                  follow=True,
+                  process_links='join_url'),
+             Rule(LinkExtractor(restrict_xpaths=('//div[@class="w-zx-nr-tj xal-bot"]/div[@class="xfg-bot1 xal-bot1"]/ul/li[@class="xfg-bot13"]/a/@href')),
+                  callback='parse_list',
+                  process_links='join_url')
+             )
     topicid_list =[{'id':'20150',
                     'name':'非法吸收公众存款罪'},
                    {'id':'20151',
@@ -56,13 +64,28 @@ class UniformSpider(Spider):
                    ]
     topic_url = 'http://www.lawxp.com/case/?TopicId=%s&WriteType=-1'
 
+    def join_url(self,links):
+        """处理提取的链接"""
+        base_url = 'http://www.lawxp.com/case/'
+        return [base_url+link for link in links]
+
     def start_requests(self):
         """从案由类出发"""
         for topic in self.topicid_list:
             print '准备采集案由分类----------------%s-----------------'%(topic['name'])
-            yield Request(url=self.topic_url%(topic['id']),callback=self.parse)
+            yield self.make_request_from_url(topic%(topic['id']))
 
-    def parse(self,response):
-        """"""
-
+    def parse_list(self,response):
+        """解析列表页"""
+        # 获取当前分类下搜索结果数量
+        quantity = Selector(response=response).xpath('//span[@class="xfg-yxtj4"]').re('\d{1,}')[0]
+        if int(quantity) < 1:
+            print '当前分类下搜索结果为0或者解析列表页出现错误,请检查--------%s---------'%(response.url)
+        # 计算最大页码
+        max_page = int(quantity)/10+int(quantity)%10
+        if max_page > 40:
+            max_page = 40 # 最大能够处理的页面为40页
+            print '当前分类下搜索结果超过40页,默认按照40页处理,请检查---------%s--------'%(response.url)
+        # 解析列表项
+        
 
